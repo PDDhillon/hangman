d81bb6da25f85eea05caf8a3ab63223ab089c087
PDDhillon <pavdhillon97@gmail.com>
2023-03-21 16:30:46 +0000

    Task 4: Check that the input is a single character

--- milestone_2.py
+++ milestone_2.py
@@ -3,4 +3,7 @@ word_list = ["lemon", "lime", "mango", "pineapple", "passionfruit"]
 word = random.choice(word_list)
 print(word)
 guess = input("Please enter a single letter.")
-
+if(len(guess) == 1 and str.isalpha(guess)):
+    print("Good Guess")
+else:
+    print("Oops! That is not a valid input")
