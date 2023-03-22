7c9cceb7d9657a74558cf07a13d08635248fabda
PDDhillon <pavdhillon97@gmail.com>
2023-03-22 19:10:02 +0000

    Milestone 3 - Task 3

--- milestone_3.py
+++ milestone_3.py
@@ -2,14 +2,21 @@ import random
 word_list = ["lemon", "lime", "mango", "pineapple", "passionfruit"]
 word = random.choice(word_list)
 print(word)
-while True:
-    guess = input("Please enter a single letter")
-    if(len(guess) == 1 and str.isalpha(guess)):
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    while True:
+        guess = input("Please enter a single letter")
+        if(len(guess) == 1 and str.isalpha(guess)):
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+ask_for_input()
